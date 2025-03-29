2a4eb2c12182f00e3a57da75e6d287d2fab612fb
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-03-29 10:58:01 +0530

    feat: complete navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import NavBar from "./components/NavBar"
-
+import { Routes , Route } from "react-router-dom"
+import Home from "./pages/Home"
 function App() {
 
 
 	return (
 
 		<>
-		<NavBar></NavBar>	
+			<NavBar></NavBar>
+			<Routes>
+				<Route path="/" element={<Home/>}></Route>
+			</Routes>
 		</>
 	)
 }
