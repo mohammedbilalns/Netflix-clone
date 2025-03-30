ba7a46b67489829cd42cdb78fd1c6489ba0369a5
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-03-30 22:52:49 +0530

    feat: add login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,55 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <></>;
+  return (
+    <>
+      <div className="w-full h-screen relative">
+        {/* Background Image */}
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_large.jpg"
+          srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_small.jpg 959w"
+          alt=""
+          className="absolute w-full h-full object-cover"
+        />
+        {/* overlay */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-10" />
+        <div className="absolute w-full px-4 py-24 z-20">
+          {/* form container */}
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white p-8 rounded">
+            <div className="mx-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Log In</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Log In
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2 " type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8 ">
+                  <span className="text-gray-600">New to Netflix</span>{" "}
+                  <Link to="/signup">Sign Up</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
